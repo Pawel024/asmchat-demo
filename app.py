56a70ca72c5fb322e2d8db970f1002cc944165d0
Pawel024 <p.j.rok@student.tudelft.nl>
2024-11-17 15:51:32 -0800

    an experiment

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 from utils.routes import init_routes
 import os
+from phoenix.otel import register
+
+tracer_provider = register(
+  project_name="default",
+  endpoint="https://app.phoenix.arize.com/v1/traces"
+)
 
 app = Flask(__name__, static_folder='./static', static_url_path='/')
 CORS(app)
