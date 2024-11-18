69214bc5ed2795f95ed262030471c819f9de3fa4
Pawel024 <p.j.rok@student.tudelft.nl>
2024-11-17 17:27:27 -0800

    revert some of the latest changes

--- app.py
+++ app.py
@@ -2,24 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 from utils.routes import init_routes
 import os
-from openinference.semconv.resource import ResourceAttributes
+from phoenix.otel import register
 from openinference.instrumentation.llama_index import LlamaIndexInstrumentor
-from opentelemetry import trace as trace_api
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
-from opentelemetry.sdk import trace as trace_sdk
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 
-resource = Resource(attributes={
-    ResourceAttributes.PROJECT_NAME: "default"
-})
-tracer_provider = trace_sdk.TracerProvider(resource=resource)
-span_exporter = OTLPSpanExporter(endpoint="http://phoenix:6006/v1/traces")
-span_processor = SimpleSpanProcessor(span_exporter=span_exporter)
-tracer_provider.add_span_processor(span_processor=span_processor)
-trace_api.set_tracer_provider(tracer_provider=tracer_provider)
+tracer_provider = register(
+  project_name="default",
+  endpoint="https://app.phoenix.arize.com/v1/traces"
+)
 
-LlamaIndexInstrumentor().instrument()
+LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
 
 app = Flask(__name__, static_folder='./static', static_url_path='/')
 CORS(app)
