fa7f4875b38476781f037f9b95b4c0c31f07e14e
Pawel024 <p.j.rok@student.tudelft.nl>
2024-11-18 17:39:58 -0800

    reverted wandb experiment

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ import os
 from phoenix.otel import register
 from openinference.instrumentation.llama_index import LlamaIndexInstrumentor
 from openinference.instrumentation.openai import OpenAIInstrumentor
-from llama_index.core.callbacks import CallbackManager
-from llama_index.callbacks.wandb import WandbCallbackHandler
-from llama_index.core import Settings
 
 tracer_provider = register(
   project_name="default",
@@ -16,14 +13,6 @@ tracer_provider = register(
 
 LlamaIndexInstrumentor().instrument(tracer_provider=tracer_provider)
 OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
-
-# initialise WandbCallbackHandler and pass any wandb.init args
-wandb_args = {"project":"asm-chat"}
-wandb_callback = WandbCallbackHandler(run_args=wandb_args)
-
-# pass wandb_callback to the settings
-callback_manager = CallbackManager([wandb_callback])
-Settings.callback_manager = callback_manager
 
 app = Flask(__name__, static_folder='./static', static_url_path='/')
 CORS(app)
